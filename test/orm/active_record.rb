f560049230611e9bc7b3d3b53ee4c36d6b99d205
Glenn Roberts <glenn.roberts@siyelo.com>
2010-04-13 09:05:27 +0200

    add intentional mistake to test CI server

--- test/orm/active_record.rb
+++ test/orm/active_record.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../../rails_app/config/environment', __FILE__)
 require 'rails/test_help'
 
+make me barf!!
+
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
 
